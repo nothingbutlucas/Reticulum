d200111ca292ec899aaf1bdaa9237d1f67494186
nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>
2024-05-05 21:45:59 -0300

    docs(script): Add script for new translations
    This will allow us to create to duplicate the file for generating a new translation
    
    Signed-off-by: nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>

---
+++ docs/add_new_translation.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+origin_directory="./source/"
+destiny_directory="./source/"
+
+echo -e "\n"
+
+# Ask the user for the new translation language
+
+read -r -p "Enter the new translation language [es, en, fr, etc]: " new_language
+
+if [[ -z "$new_language" ]]; then
+	echo -e "Invalid language code"
+	exit 1
+fi
+
+echo "Duplicating files for $new_language language"
+for file in "$origin_directory"/*; do
+	# Took the original files and duplicate them with the new language
+	if [[ ! "$file" =~ _ ]]; then
+		# Parse name and extesion
+		name=$(basename "$file")
+		name_without_extension="${name%.*}"
+		extension="${name##*.}"
+		if [[ "$extension" == "rst" ]]; then
+			new_name="${name_without_extension}_${new_language}.${extension}"
+			# Duplicate the file
+			cp "$file" "$destiny_directory/$new_name"
+		fi
+	fi
+done
+
+echo -e "Done, happy translation!\n"
